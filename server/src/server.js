68cf5da87d78a4645ee9c137c657eb974e403317
afrahalharthy11-ship-it <afrahalharthy11@gmail.com>
2026-05-14 21:42:18 -0700

    fix arrow final

--- server/src/server.js
+++ server/src/server.js
@@ -49,8 +49,7 @@ app.use("/api/manager", managerRoutes);
 
 // error handler
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
+app(err.stack);app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
@@ -59,4 +58,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
