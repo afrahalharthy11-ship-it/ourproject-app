7dc825a816629cbd667b66c619fbd0613d66cca0
afrahalharthy11-ship-it <afrahalharthy11@gmail.com>
2026-05-14 20:54:20 -0700

    fix server crash

--- server/src/server.js
+++ server/src/server.js
@@ -48,6 +48,7 @@ app.use("/api/patient", patientRoutes);
 app.use("/api/manager", managerRoutes);
 
 // error handler
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Internal server error" });
@@ -58,3 +59,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
